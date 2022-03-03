8190dfb0783643741fa707a61af6840c7f1605dd
fjost <jost@mobile-software.ag>
2022-03-03 15:36:31 +0100

    feat: add dialog to lib

--- projects/feedback-tool-lib/src/lib/feedback-tool-lib.service.ts
+++ projects/feedback-tool-lib/src/lib/feedback-tool-lib.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Dialog } from '@capacitor/dialog';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +9,17 @@ export class FeedbackToolLibService {
   private readonly api = 'https://catfact.ninja/fact';
 
   constructor(private http: HttpClient) {}
+
+
   getCatFact() {
     return this.http
-      .get<{ fact: string; length: number }>(this.api)
-      .pipe(map((res) => res.fact));
+      .get<{ fact: string; length: number }>(this.api);
+  }
+
+  async showDialog() {
+    await Dialog.alert({
+      title: 'Hello',
+      message: 'I am working',
+    });
   }
 }
